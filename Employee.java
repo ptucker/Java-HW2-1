0cba655a27b0c2fc1836acce6a1232ca1dff44f7
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-08 18:19:01 -0800

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public interface Employee {
+    int Pay;
+    String EmployeeID; 
+//Method to give employees their pay
+public String GetPay(){
+return Pay;
+}
+//method to ask employee their empoyee ID
+public String GetEmployeeID(){
+return EmployeeID;
+   }
+    
+}
