fc3ed286071a483b0b56e8f1b035a7f73494e4dd
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-08 18:16:30 -0800

    Update PersonTest.java

--- PersonTest.java
+++ PersonTest.java
@@ -1,1 +1,12 @@
+public class PersonTest {
+    public static void main(String[] args) {
+    Person person1 = new Person(); // new person object
+    person1.setName ("Mia");
+    person1.setAge(21);
+    person1.setPhoneNumber(520665656);
 
+    System.out.printf( person1.toString());
+System.out.printf(" Does this person work as a teacher, with the police or is this person a kid? ");
+
+    }
+}
