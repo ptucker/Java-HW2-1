61d950ba7e3919800be55d422f4988ea6ff3b7ae
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-08 18:16:14 -0800

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,1 +1,26 @@
 
+public class Person {
+    private String Name;
+    private int Age;
+    public int PhoneNumber;
+
+
+//toString will return something later in PersonTest
+   
+    public void setName (String newName)
+    {
+        Name = newName;
+    }
+    public void setAge (int newAge)
+    {
+        Age = newAge;
+    }
+    public void setPhoneNumber ( int newPhoneNumber)
+    {
+        PhoneNumber = newPhoneNumber;
+    }
+    
+    public String toString(){
+        return Name + " is " + Age + " years old. " + Name  + " has the number: " + PhoneNumber;
+    }
+}
