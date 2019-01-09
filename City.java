4ab1fd2d1686c2c8af7f68d4e7fb9939837dfe74
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-08 18:17:38 -0800

    Create City.java

---
+++ City.java
@@ -0,0 +1,6 @@
+public class City { 
+//You should be able to output the names of all people in a loop
+//You should be able to output the names of all buildings in a loop
+//You should know which kids and teachers are in school, and which police officers are in City Hall
+//You should be able to pay those persons who are employees  
+}
