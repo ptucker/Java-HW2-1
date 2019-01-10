41da6bfa1ce881bd605d156f5e3ff9c400283f11
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-09 21:16:32 -0800

    Update School.java

--- School.java
+++ School.java
@@ -1,6 +1,7 @@
 public class School extends Building {
     
-     Person CurInSchool[] = new Person [1];// Arrays of current occupants 
+     ArrayList<School> CurInSchool = new ArrayList <>();// Arrays of current occupants
+
 
      //write loops here to let me know of all the people in school
 }
