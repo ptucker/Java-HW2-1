1540c5aae8d58ee4f446f4273bb69759b852f866
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-09 16:38:37 -0800

    Update Kid.java

--- Kid.java
+++ Kid.java
@@ -1,6 +1,13 @@
-
 public class Kis extends Person{
     //favorite candy
     String FavCandy;
-    
+    public void SetFavCandy(String Fav)
+    {
+        FavCandy = Fav;
+    }
+    public getFavCandy()
+    {
+        return FavCandy;
+    }
+  
 }
