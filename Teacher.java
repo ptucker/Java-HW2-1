6858c2e9180a1fafb22f4bea6a119c7cef7af1d0
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-09 16:35:00 -0800

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,5 +1,7 @@
 public class Teacher extends Person implement Employee {
     String GradeLevel;
     String certification;
-    
+
+boolean IsATeacher(Employee t){return true;}
+boolean IsApoliceman(Employee p){return false;}
 }
