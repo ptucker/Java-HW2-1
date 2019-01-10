4c3ef8310269beb04c719662b2df3869cf5c5fe6
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-09 16:40:18 -0800

    Update Building.java

--- Building.java
+++ Building.java
@@ -1,4 +1,15 @@
-public class Building{
+public class Building {
     String BuildingName;
     String BuildingAddress;
+
+public String getBuildingName()
+{
+    return BuildingName;
+}
+public String getBuildingAddress()
+{
+    return BuildingAddress;
+}
+
+    
 }
