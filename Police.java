aa349b5eba6a0f3e8a8b308ebe0ffb3e6bf5be73
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-09 16:37:31 -0800

    Update Police.java

--- Police.java
+++ Police.java
@@ -1,5 +1,7 @@
-
 public class Police extends Person implement Employee {
     enum Role {Patrol, Sargent, Captain, Cheif;}
     // this person is in a... 
+boolean IsATeacher(Employee t){return false;}
+boolean IsApoliceman(Employee p){return true;}
 }
+
