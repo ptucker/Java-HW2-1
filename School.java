015455093b53dd91dea191bb0a5b2563de95688e
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-10 16:37:20 -0800

    Update School.java

--- School.java
+++ School.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
 public class School extends Building {
+    ArrayList<Person> CurInSchool = new ArrayList <>();// Arrays of current occupant
+
+    private String SchoolName;
+    private String SchoolAddress; 
+
+    public School() {
+        this.SchoolName = "north school";
+        this.SchoolAddress = "300 r";
+    }
+
+public String getSchoolName()
+{
+    return this.SchoolName;
+}
+public String getSchoolAdress()
+{
+    return this.SchoolAddress;
+}
+public void setSchoolAdress(String Saddress)
+{
+this.SchoolAddress = Saddress;
+}
+public void setSchoolName(String Sname)
+{
+this.SchoolName = Sname;
+}
     
-     ArrayList<School> CurInSchool = new ArrayList <>();// Arrays of current occupants
+      
 
+//write a constructor 
 
-     //write loops here to let me know of all the people in school
 }
