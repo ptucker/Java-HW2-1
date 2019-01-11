18bb2916098b68014617698056511cc4eab44dd8
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-10 16:40:04 -0800

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -2,17 +2,21 @@ public interface Employee {
 
     boolean IsATeacher(Employee t);
     boolean IsApoliceman(Employee p);
+    //int Money;
+    //String EmployeeID;
+
+    // constructor
+   // Employee (int Money, String EmployeeID)
+    //{
+        //this.NewMoney = Money;
+      //  this.NewEmployeeID = EmployeeID;
+    //} 
+//------------------------errors from int Money and String EmployeeID
 
 
-    int Money;
-    String EmployeeID; 
 //Method to give employees their pay
-public String GetPay(){
-return Money;
-}
+public String GetPay();
 //method to ask employee their empoyee ID
-public String GetEmployeeID(){
-return EmployeeID;
-   }
+public String GetEmployeeID();
     
 }
