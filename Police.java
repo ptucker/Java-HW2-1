841d7821dcecb1f05bd88ecf647f8745bd824a8f
Peter Tucker <ptucker@whitworth.edu>
2019-01-11 07:59:13 -0800

    feedback

--- Police.java
+++ Police.java
@@ -1,6 +1,13 @@
+//PT -- missing javadoc. -2
+
 public class Police extends Person implement Employee {
     enum Role {Patrol, Sargent, Captain, Cheif;}
     // this person is in a... 
+    //PT -- Maybe make a Role member variable to store each police person's position?
+    //PT -- Role r;
+    //PT public Police(Role role) { r = role; }
+    
+    //PT -- I think you don't need to take parameters for these functions, since the code for these functions doesn't use them.
 boolean IsATeacher(Employee t){return false;}
 boolean IsApoliceman(Employee p){return true;}
 }
