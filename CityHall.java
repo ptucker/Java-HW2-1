1b5b5befe2c3a0300ea65993f0f77d1c90163b67
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-10 16:37:38 -0800

    Update CityHall.java

--- CityHall.java
+++ CityHall.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
 public class CityHall extends Building {
-  
-    ArrayList<CityHall> CurInCityHall = new ArrayList <>();//array of current occupant(s)
+ArrayList<Person> CurInCityHall = new ArrayList <>();//array of current occupant(s)
 
-  //write loops here to let me know of all the people in CityHall.. 
+private String CityHallName;
+private String CityHallAddress;
+//Constructor 
+   public CityHall ()  {
+     this.CityHallName = "north city hall";
+     this.CityHallAddress = " 1235";
+   }
+
+public String getCityHallName()
+{
+    return this.CityHallName;
+}
+public String getCityHalllAdress()
+{
+    return this.CityHallAddress;
+}
+public void setCityHallAddress(String Caddress)
+{
+this.CityHallAddress = Caddress;
+}
+public void setCityHallName(String Cname)
+{
+this.CityHallName = Cname;
+}
+  // loops here to let me know of all the people in CityHall.. 
 }
