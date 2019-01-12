c83cfc18a0b22b4431f9292199b15205b7e8064b
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-11 16:05:41 -0800

    Update Kid.java

--- Kid.java
+++ Kid.java
@@ -1,4 +1,9 @@
-public class Kis extends Person{
+/**
+* This class models a Kid's favorite candy
+* @author Karen Sobtafo
+* @version 1.00, 11 January 2019
+*/
+public class Kid extends Person{
     //favorite candy
     String FavCandy;
     public void SetFavCandy(String Fav)
