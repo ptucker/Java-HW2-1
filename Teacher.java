e6c76288f87be26799bababda91f1999f5b2140b
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-11 16:03:22 -0800

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,7 +1,29 @@
-public class Teacher extends Person implement Employee {
+/**
+* This class models a teacher's grade level and certification
+* @author Karen Sobtafo
+* @version 1.00, 11 January 2019
+*/
+public class Teacher extends Person implements Employee {
     String GradeLevel;
     String certification;
+    
+int Pay;
+String EmployeeID;
 
-boolean IsATeacher(Employee t){return true;}
+public int GetPay()
+ {
+    return Pay;
+ }
+
+ public void SetPay (int payy)
+ {
+    Pay = payy;
+ }
+
+public String GetEmployeeID()
+{
+    return EmployeeID;
+}
 
 }
+
