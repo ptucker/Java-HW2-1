dd482b6cfcf505998b6de99047fdc80e22e457c2
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-11 16:06:31 -0800

    Update CityHall.java

--- CityHall.java
+++ CityHall.java
@@ -1,30 +1,30 @@
+/**
+* This class models a CityHall's name and address
+* @author Karen Sobtafo
+* @version 1.00, 11 January 2019
+*/
 import java.util.ArrayList;
 public class CityHall extends Building {
 ArrayList<Person> CurInCityHall = new ArrayList <>();//array of current occupant(s)
 
-private String CityHallName;
-private String CityHallAddress;
-//Constructor 
-   public CityHall ()  {
-     this.CityHallName = "north city hall";
-     this.CityHallAddress = " 1235";
-   }
+//Constructor inherited form building
+   public CityHall ()  { super("north city hall", "1235"); }
 
-public String getCityHallName()
-{
-    return this.CityHallName;
+//method to search for people in CityHall
+public int Search(Person p){
+   for (int i =0; i<CurInCityHall.size();i++)
+   {
+  CurInCityHall.get(i);
+
+if (CurInCityHall.get(i).getName() == p.getName())
+  { return i;}
+
+   }
+   //else return default number that is not an index
+   return -1;
+
 }
-public String getCityHalllAdress()
-{
-    return this.CityHallAddress;
-}
-public void setCityHallAddress(String Caddress)
-{
-this.CityHallAddress = Caddress;
-}
-public void setCityHallName(String Cname)
-{
-this.CityHallName = Cname;
-}
-  // loops here to let me know of all the people in CityHall.. 
+
+
+
 }
