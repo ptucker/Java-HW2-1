7b42b30b995ddcafe9f0fb40f3544cd812ecd07b
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-11 16:03:41 -0800

    Update School.java

--- School.java
+++ School.java
@@ -1,34 +1,27 @@
+/**
+* This class models a School's name and address
+* @author Karen Sobtafo
+* @version 1.00, 11 January 2019
+*/
 import java.util.ArrayList;
 public class School extends Building {
     ArrayList<Person> CurInSchool = new ArrayList <>();// Arrays of current occupant
 
-    private String SchoolName;
-    private String SchoolAddress; 
+// constructor that inherits from building
+    public School() {super ("north school", "300 r");}
+//method to search for people in School
+public int SearchSchool(Person q){
+   for (int i =0; i<CurInSchool.size();i++)
+   {
+  CurInSchool.get(i);
 
-    public School() {
-        this.SchoolName = "north school";
-        this.SchoolAddress = "300 r";
-    }
+if (CurInSchool.get(i).getName() == q.getName())
+  { return i;}
 
-public String getSchoolName()
-{
-    return this.SchoolName;
+   }
+   //else return default number that is not an index
+   return -1;
 }
-public String getSchoolAdress()
-{
-    return this.SchoolAddress;
-}
-public void setSchoolAdress(String Saddress)
-{
-this.SchoolAddress = Saddress;
-}
-public void setSchoolName(String Sname)
-{
-this.SchoolName = Sname;
-}
-    
-      
-
-//write a constructor 
 
 }
+
